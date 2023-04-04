cd9ce90b7097deb43b890db342adbba1a8f19edb
Лана Константинова <lnk.konst@gmail.com>
2023-04-04 04:32:40 +0300

    clean webpack plugin

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 
@@ -26,6 +27,7 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'style.css'
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
 };
