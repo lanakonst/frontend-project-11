9683b9c127e68d8291d6c9b151f816ee7867dfd2
Лана Константинова <lnk.konst@gmail.com>
2023-04-04 05:22:40 +0300

    adding jquery to webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 // @ts-check
-import 'jquery';
-import 'popper.js';
-
 import './style.css';
 // import './styles.scss';
 // import 'bootstrap';
 
 console.log('HI');
-console.log('HEEEY');
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 
 
@@ -28,6 +30,11 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'style.css'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    })
   ],
 };
